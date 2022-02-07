7d62b6e68cd725c09e9f8af11f23d38ee9e0b70e
Abdikadirdahir <61722709+Abdikadirdahir@users.noreply.github.com>
2022-02-07 12:35:28 -0800

    todo-list upgraded

---
+++ todo.js
@@ -0,0 +1,73 @@
+var todoList = document.querySelector("#todo-list");
+var todoInput = document.querySelector("#todo-text");
+var todoCount = document.querySelector("#todo-count")
+var todoForm = document.querySelector("#todo-form") 
+
+
+
+
+
+var todos = []
+
+
+function renderTodos( ) {
+todoList.innerHTML = "";
+
+todoCount.textContent = todos.length;
+
+for (var i = 0; i < todos.length; i++) {
+
+    var todo = todos[i];
+    var li = document.createElement("li")
+    li.textContent = todo;
+    li.setAttribute("date-index", i)
+    
+    var button = document.createElement("button")
+    button.textContent = " ☑️ "
+    li.appendChild(button)
+    todoList.appendChild(li)
+}
+} 
+
+
+  
+console.log(renderTodos)
+
+function init() {
+    var storeTodos = JSON.parse(localStorage.getItem("todos"))
+    if(storeTodos !== null){
+        todos =storeTodos;
+    }
+    renderTodos()
+}
+
+function storeTodos( ) {
+    localStorage.setItem("todos", JSON.stringify(todos))
+}
+
+todoForm.addEventListener("submit", function(event) {
+   event.preventDefault();
+   
+   var todoText = todoInput.value.trim()
+   if(todoText === " ") {
+       return;
+   }
+   todos.push(todoText);
+   todoInput.value = " "
+
+   storeTodos()
+   renderTodos()
+}) 
+
+todoList.addEventListener("click", function(event){
+    var element =event.target;
+    if (element.matches("button") === true) {
+       var index = element.parentElement.getAttribute("data-index");
+       todos.splice(index,1)
+       storeTodos()
+       renderTodos()
+    }
+    
+})
+
+init() 
